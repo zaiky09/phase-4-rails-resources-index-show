829c3e3876865655e6aec531a45971d5cd5e4827
zaiky09 <zamilmozamil@outlook.com>
2023-03-20 13:40:45 +0300

    done the index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #show list of all birds
+  # get '/birds', to: 'birds#index'
+
+  #show one specific bird
+  # get '/birds/:id', to: 'birds#show'
+
+  resources :birds, only: [:index, :show]
+
 end
